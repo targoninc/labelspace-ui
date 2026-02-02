0e052a27964014f3174ceb62b003cd6636f0936e
alex <alex@targoninc.com>
2026-02-02 17:18:39 +0100

    layout

--- src/ui/code/components/generic/images.ts
+++ src/ui/code/components/generic/images.ts
@@ -2,24 +2,24 @@ import {getImageUrl} from "../../functions/templates.ts";
 import {MediaFileType} from "../../enums/MediaFileType.ts";
 import {RequestableImageSize} from "../../enums/requestableImageSize.ts";
 import {ImageSize} from "../../enums/imageSize.ts";
-import { uploadImage } from "../../functions/media.ts";
-import { Generics } from "./generics.ts";
-import { Api } from "../../api/api.ts";
-import { NotificationType } from "../../enums/NotificationType.ts";
-import { notify } from "../../functions/notifications.ts";
+import {uploadImage} from "../../functions/media.ts";
+import {Generics, horizontal} from "./generics.ts";
+import {Api} from "../../api/api.ts";
+import {NotificationType} from "../../enums/NotificationType.ts";
+import {notify} from "../../functions/notifications.ts";
 import {create, getValue, signal, Signal, when} from "@targoninc/jess";
 import {button} from "@targoninc/jess-components";
 
 interface ChangeableImageOptions {
-    changeable?: boolean|Signal<boolean>;
-    deletable?: boolean|Signal<boolean>;
+    changeable?: boolean | Signal<boolean>;
+    deletable?: boolean | Signal<boolean>;
     afterChange?: () => void;
     size?: ImageSize;
     classes?: string[];
 }
 
 export class Images {
-    static changeableImage(id: number|Signal<number>, hasImage: boolean|Signal<boolean>, type: MediaFileType, options: ChangeableImageOptions, fallback: string = "") {
+    static changeableImage(id: number | Signal<number>, hasImage: boolean | Signal<boolean>, type: MediaFileType, options: ChangeableImageOptions, fallback: string = "") {
         const src = signal(fallback);
         if (hasImage instanceof Signal) {
             (hasImage as Signal<boolean>).subscribe(h => {
@@ -32,35 +32,31 @@ export class Images {
         }
         const loading = signal(false);
 
-        return create("div")
-            .classes("flex-v")
-            .children(
-                Images.image(src, options.size ?? ImageSize.adaptive, options.classes),
-                create("div")
-                    .classes("flex")
-                    .children(
-                        when(options.changeable, button({
-                            icon: { icon: "upload" },
-                            text: "Change",
-                            disabled: loading,
-                            onclick: () => {
-                                uploadImage(loading, type, getValue(id));
-                            }
-                        })),
-                        when(options.deletable, button({
-                            icon: { icon: "delete" },
-                            text: "Delete",
-                            disabled: loading,
-                            onclick: () => {
-                                loading.value = true;
-                                Api.deleteMedia(type, getValue(id)).then(() => {
-                                    notify("Deleted media", NotificationType.success);
-                                }).finally(() => loading.value = false);
-                            }
-                        })),
-                        when(loading, Generics.loading())
-                    ).build()
-            ).build();
+        return horizontal(
+            Images.image(src, options.size ?? ImageSize.adaptive, options.classes),
+            horizontal(
+                when(options.changeable, button({
+                    icon: {icon: "upload"},
+                    text: "Change",
+                    disabled: loading,
+                    onclick: () => {
+                        uploadImage(loading, type, getValue(id));
+                    }
+                })),
+                when(options.deletable, button({
+                    icon: {icon: "delete"},
+                    text: "Delete",
+                    disabled: loading,
+                    onclick: () => {
+                        loading.value = true;
+                        Api.deleteMedia(type, getValue(id)).then(() => {
+                            notify("Deleted media", NotificationType.success);
+                        }).finally(() => loading.value = false);
+                    }
+                })),
+                when(loading, Generics.loading())
+            ).build()
+        ).build();
     }
 
     private static setSrc(options: ChangeableImageOptions, id: number, src: Signal<string>, type: MediaFileType) {
